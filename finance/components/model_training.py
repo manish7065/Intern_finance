b0dc357156057d593b78614c5aaf89af312a036a
manish <manumi296@gmail.common>
2023-05-07 02:18:19 +0530

    stock selection option added

---
+++ finance/components/model_training.py
@@ -0,0 +1,2 @@
+from finance.logger import logging
+from finance.exception import FinanceException
--- finance/entity/artifact_entity.py
+++ finance/entity/artifact_entity.py
@@ -7,4 +7,9 @@ class DataIngestionArtifact:
     trained_file_path:str  #path to save train data
     test_file_path:str     #path to save testing data
 
+@dataclass
+class StockSelection:
+
+    stock_selected:str  #User selected stock 
+
 
