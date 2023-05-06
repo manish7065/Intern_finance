9b7204ca73552aa44ecd72ac461faf3adcfeab49
manish <manumi296@gmail.common>
2023-05-07 02:14:29 +0530

    pipeline status condition added

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,Response
+from finance.entity.artifact_entity import StockSelection
+from finance.pipeline import TrainPipeline
+from finance.exception import FinanceException
 
 app = Flask(__name__)
 
@@ -8,11 +11,21 @@ def index():
 
 @app.route("/result", methods=["POST"])
 def result():
-    data = request.form
-    option = data["options"]
-    print(f'getting the data: {option}')
+    try:
+        data = request.form
+        option = data["options"]
+        StockSelection.stock_selected = option
+        print(f'getting the data: {option}')
 
-    return render_template("result.html", option=option)
+        trian_pipeline = TrainPipeline()
+        if trian_pipeline.is_pipeline_running:
+            return Response("Training pipeline is running")
+        trian_pipeline.run_pipeline()
+
+        return render_template("result.html", option=option)
+    except Exception as e:
+        return Response(f" Error ocuured! {e}")
+        raise FinanceException(e, sys)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
